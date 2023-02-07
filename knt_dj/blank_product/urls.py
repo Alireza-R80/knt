4d0e63f7b5051b093fb159b533aafbe1c9ba19b4
Alireza-R80 <mr.alirezarezaei.1380@gmail.com>
2023-02-07 12:19:54 -0500

    blank product views and urls added.

--- knt_dj/blank_product/urls.py
+++ knt_dj/blank_product/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
-
-from blank_product.views import BlankProductListView
+from .views import *
 
 router = routers.DefaultRouter()
-router.register('', BlankProductListView)
+router.register('type', BlankProductTypeViewSet)
+router.register('prop', BlankProductPropViewSet)
+router.register('prop_value', BlankProductPropValueViewSet)
+router.register('image', BlankProductImageViewSet)
+router.register('sample_image', BlankProductSampleImageViewSet)
+router.register('', BlankProductViewSet)
+
 urlpatterns = [
+    path('prop/find_by_blank_product/<int:bpID>', PropByBlankProductListView.as_view(), name='blank_product_prop'),
+    path('find_by_category/<slug:slug>', BlankProductByCategoryListView.as_view(), name='category_item'),
+    path('available/', AvailableBlankProductListView.as_view(), name='available_blank_product'),
+    path('prop_value/find_by_blank_product/<int:bpID>', PropValueByBlankProductListView.as_view(),
+         name='blank_product_prop_value'),
     path('', include(router.urls)),
 ]
--- knt_dj/blank_product/views.py
+++ knt_dj/blank_product/views.py
@@ -1,12 +1,65 @@
-from rest_framework import viewsets ,generics
-from rest_framework.permissions import AllowAny
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from rest_framework import generics
 
-from blank_product.models import BlankProduct
-from blank_product.serializers import BlankProductSerializer
+
+class BlankProductTypeViewSet(viewsets.ModelViewSet):
+    queryset = BlankProductType.objects.all()
+    serializer_class = BlankProductTypeSerializer
 
 
-class BlankProductListView(viewsets.ModelViewSet):
+class BlankProductPropViewSet(viewsets.ModelViewSet):
+    queryset = BlankProductProp.objects.all()
+    serializer_class = BlankProductPropSerializer
+
+
+class PropByBlankProductListView(generics.ListAPIView):
+    serializer_class = BlankProductPropSerializer
+
+    def get_queryset(self):
+        return BlankProductProp.objects.filter(blank_product__id=self.kwargs['bpID'])
+
+
+class BlankProductViewSet(viewsets.ModelViewSet):
     queryset = BlankProduct.objects.all()
     serializer_class = BlankProductSerializer
-    permission_classes = (AllowAny,)
 
+
+class BlankProductByCategoryListView(generics.ListAPIView):
+    serializer_class = BlankProductSerializer
+
+    def get_queryset(self):
+        return BlankProduct.objects.filter(
+            category__in=Category.objects.get(slug=self.kwargs['slug']).get_descendants(include_self=True))
+
+
+class AvailableBlankProductListView(generics.ListAPIView):
+    serializer_class = BlankProductSerializer
+
+    def get_queryset(self):
+        return BlankProduct.objects.filter(is_available=True)
+
+
+class BlankProductPropValueViewSet(viewsets.ModelViewSet):
+    queryset = BlankProductPropValue.objects.all()
+    serializer_class = BlankProductPropValueSerializer
+
+
+class PropValueByBlankProductListView(generics.ListAPIView):
+    serializer_class = BlankProductPropValueSerializer
+
+    def get_queryset(self):
+        return BlankProductPropValue.objects.filter(blank_product__id=self.kwargs['bpid'])
+
+
+class BlankProductImageViewSet(viewsets.ModelViewSet):
+    queryset = BlankProductImage.objects.all()
+    serializer_class = BlankProductImageSerializer
+
+
+class BlankProductSampleImageViewSet(viewsets.ModelViewSet):
+    queryset = BlankProductSampleImage.objects.all()
+    serializer_class = BlankProductSampleImageSerializer
+
+# Create your views here.
