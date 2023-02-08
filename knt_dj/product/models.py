7b69d8f6f62d58b189e7bc3a5fb23e06709edb29
Adel <adel13no@gmail.com>
2023-02-08 18:44:04 +0330

    models modification, utils app created

--- knt_dj/product/models.py
+++ knt_dj/product/models.py
@@ -3,6 +3,7 @@ from django.db.models.aggregates import Min
 
 from account.models import *
 from blank_product.models import BlankProduct, BlankProductType
+from utils.models import Color, Size
 
 
 class Product(models.Model):
@@ -58,18 +59,6 @@ class ProductTag(models.Model):
         return self.name
 
 
-class Color(models.Model):
-    name = models.CharField(max_length=20)
-    code = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
-
-class Size(models.Model):
-    type = models.ForeignKey(BlankProductType, on_delete=models.CASCADE, related_name='sizes')
-    unit = models.CharField(max_length=10)
-
 
 # to see if product with color n exists with size m and how much is the price for each combination
 class ProductDetail(models.Model):
@@ -82,28 +71,4 @@ class ProductDetail(models.Model):
         return self.product
 
 
-class ProductProviderProp(models.Model):
-    blank_product = models.ForeignKey(BlankProduct, on_delete=models.CASCADE, related_name='ppp')
-    provider = models.ForeignKey(
-        PrintProvider,
-        on_delete=models.CASCADE,
-        null=True,
-        related_name='product_provider_prop')
-    prep_time = models.CharField(max_length=20)
 
-    def __str__(self):
-        return f'{self.blank_product} - {self.provider}'
-
-    def get_min_price(self):
-        min_price = ProductProviderDetail.objects.filter(product_provider_prop=self).aggregate(min_price=Min('price'))
-        return min_price
-
-
-class ProductProviderDetail(models.Model):
-    product_provider_prop = models.ForeignKey(ProductProviderProp, on_delete=models.CASCADE, related_name='ppd')
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL, related_name='ppd', null=True)
-    size = models.ForeignKey(Size, on_delete=models.SET_NULL, related_name='ppd', null=True)
-    price = models.FloatField()
-
-    def __str__(self):
-        return self.product_provider_prop
